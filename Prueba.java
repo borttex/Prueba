49dc28559e85142cf57ae0b040e1c3842bd36cc4
borttex <bbangcooper@gmail.com>
2014-06-05 00:30:08 +0200

    Create Prueba.java

---
+++ Prueba.java
@@ -0,0 +1,29 @@
+public class Prueba{
+
+int id;
+String nombre;
+int puntos;
+
+public void getId(){
+  
+  return this.id;
+}
+
+public void getNombre(){
+  
+  return this.nombre;
+}
+public void getPuntos(){
+  
+  return this.puntos;
+}
+
+public Prueba(int id, String nombre, int puntos){
+	
+	this.id= id;
+	this.nombre=nombre;
+	this.puntos=puntos;
+	
+}
+
+}
